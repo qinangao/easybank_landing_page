936df6a902ebd7c03d7992c4f350f8563bf367ca
Qinan Gao <qinanagao@gmail.com>
2025-03-02 18:29:15 +0000

    add hover

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
 
+// Moblie Menu
+
 const btnMenuOpen = document.querySelector(".btn_menu_open");
 const btnMenuClose = document.querySelector(".btn_menu_close");
-
 const menuContainer = document.querySelector(".nav_links");
 
 btnMenuOpen.addEventListener("click", function (e) {
@@ -16,4 +17,26 @@ btnMenuClose.addEventListener("click", function () {
   menuContainer.style.display = "none";
   btnMenuClose.style.display = "none";
   btnMenuOpen.style.display = "block";
+});
+
+//Modal Window
+
+const modal = document.querySelector(".modal");
+const btnModalClose = document.querySelector(".btn-close-modal");
+const btnModalOpen = document.querySelectorAll(".btn-show-modal");
+const overlay = document.querySelector(".overlay");
+//Open modal
+
+btnModalOpen.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+});
+
+btnModalClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
