fe05e6010dad0efa7e589124d38a57c9dadc1c96
Qinan Gao <qinanagao@gmail.com>
2025-03-03 22:03:07 +0000

    add nav animation

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ const btnModalOpen = document.querySelectorAll(".btn-show-modal");
 const overlay = document.querySelector(".overlay");
 const btnSignInModalShow = document.querySelector(".btn-sign-in");
 const signInModal = document.querySelector(".sign-in-modal");
-console.log(btnModalClose);
 
 const openModal = function (e) {
   e.preventDefault();
@@ -45,7 +44,7 @@ const closeModal = function (e) {
 btnModalOpen.forEach((btn) => {
   btn.addEventListener("click", openModal);
 });
-
+overlay.addEventListener("click", closeModal);
 btnSignInModalShow.addEventListener("click", function (e) {
   e.preventDefault();
   signInModal.classList.remove("hidden");
@@ -54,4 +53,33 @@ btnSignInModalShow.addEventListener("click", function (e) {
 
 btnModalClose.forEach((btn) => {
   btn.addEventListener("click", closeModal);
+});
+
+//Navigation animation
+const nav = document.querySelector(".desktop_nav");
+
+const handleHover = function (e, opacity) {
+  if (
+    e.target.classList.contains("nav_link") ||
+    e.target.classList.contains("btn__nav")
+  ) {
+    const link = e.target;
+    const siblings = link
+      .closest(".desktop_nav")
+      .querySelectorAll(".nav_link, .btn__nav");
+    const logo = link.closest(".desktop_nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener("mouseover", function (e) {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener("mouseout", function (e) {
+  handleHover(e, 1);
 });
